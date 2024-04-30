06eb0845da7401895e7ff73c4bbb23b56c17d0ae
Kim GwangYeong <gospotb1607@gmail.com>
2024-04-30 10:42:55 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 46.55 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,18 @@
+def solution(participant, completion):
+    dic = {}
+    
+    for p in participant:
+        if p in dic:
+            dic[p] += 1
+        else:
+            dic[p] = 1
+    
+    answer = ''
+    for c in completion:
+        dic[c] -= 1
+    
+    for key in dic.keys():
+        if dic[key] > 0:
+            answer = key
+            
+    return answer
