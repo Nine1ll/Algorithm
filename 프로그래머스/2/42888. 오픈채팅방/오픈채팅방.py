a9764f4232c2e2bf9489be8773676bdce73ca473
Kim GwangYeong <gospotb1607@gmail.com>
2024-05-02 14:30:01 +0900

    [level 2] Title: 오픈채팅방, Time: 70.88 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,18 +1,17 @@
-# 닉네임은 마지막만 기억하면 되는거고, userid의 행동들은 순서대로 기억해야함.
-# userid : 행동은 순서대로 기억하되, userid : 닉네임은 마지막만 기억해서 출력.
 def solution(record):
-    uid_nick = {}
-    answer = []
-    for log in record:
-        command = log.split(" ")
-        if command[0] != "Leave":
-            uid_nick[command[1]] = command[2]
-        
-    for log in record:
-        command = log.split(" ")
-        if command[0] == "Enter":
-            answer.append(f"{uid_nick[command[1]]}님이 들어왔습니다.")
-        elif command[0] == "Leave":
-            answer.append(f"{uid_nick[command[1]]}님이 나갔습니다.")
-            
+    usr = {}
+    for line in record:
+        cmd = line.split(" ")
+        if cmd[0] != "Leave":
+            usr[cmd[1]] = cmd[2]
+    
+    answer = []            
+    for line in record:
+        cmd = line.split(" ")
+        user_name = usr[cmd[1]]
+        if cmd[0] == "Enter":
+            answer.append(f"{user_name}님이 들어왔습니다.")
+        elif cmd[0] == "Leave":
+            answer.append(f"{user_name}님이 나갔습니다.")
+
     return answer
