34ecc80191c7f2477eae9ae82d628e3eda5c3a50
Kim GwangYeong <gospotb1607@gmail.com>
2024-05-11 11:36:32 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,12 +1,16 @@
 def solution(id_list, report, k):
-    answer = [0] * len(id_list)    
-    reports = {x : 0 for x in id_list}
+    answer = [0] * len(id_list)
+    reports = {id : [[],0] for id in id_list}
+    report = set(report)
 
-    for r in set(report):
-        reports[r.split()[1]] += 1
+    for r in report:
+        user_id, report_id = r.split(" ")
+        reports[user_id][0].append(report_id)
+        reports[report_id][1] += 1
 
-    for r in set(report):
-        if reports[r.split()[1]] >= k:
-            answer[id_list.index(r.split()[0])] += 1
-
+    for index, i in enumerate(id_list):
+        for name in reports[i][0]:
+            if reports[name][1] >= k:
+                answer[index] += 1 
+                
     return answer
