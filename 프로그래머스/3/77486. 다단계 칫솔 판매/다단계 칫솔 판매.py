8f2b65cf95d49ebd268ce4df334e8d47217c21ed
Kim GwangYeong <gospotb1607@gmail.com>
2024-06-01 11:56:08 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 170.12 ms, Memory: 20.5 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.py
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.py
@@ -1,15 +1,22 @@
+def to_parents(parents, moneys, name, earn_money):
+    current = name
+    while current != "":
+        earn_for_parent = earn_money // 10
+        moneys[current] += earn_money - earn_for_parent
+        earn_money = earn_for_parent
+        current = parents[current]
+        if earn_money == 0:
+            break
+
+
 def solution(enroll, referral, seller, amount):
-    parent = dict(zip(enroll, referral))
-    total = {name : 0 for name in enroll}
+    money = {name: 0 for name in enroll}
+    money["-"] = 0
+    parent = {enroll[i]: referral[i] for i in range(len(enroll))}
+    parent["-"] = ""
 
     for i in range(len(seller)):
-        money = amount[i] * 100
-        cur_name = seller[i]
+        earn = amount[i] * 100
+        to_parents(parent, money, seller[i], earn)
 
-        while money > 0 and cur_name != "-":
-            total[cur_name] += money - money // 10
-            cur_name = parent[cur_name]
-
-            money //= 10
-
-    return [total[name] for name in enroll]
+    return [money[name] for name in enroll]
