1c0ec89edd1918eb26ae81f1b887de07d13a5878
Kim GwangYeong <gospotb1607@gmail.com>
2024-07-22 10:35:28 +0900

    [Silver III] Title: 주유소, Time: 112 ms, Memory: 41844 KB, Score: 17 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -0,0 +1,15 @@
+import sys
+
+n = int(sys.stdin.readline())
+length = list(map(int, sys.stdin.readline().split()))
+price = list(map(int, sys.stdin.readline().split()))
+
+money = 0
+for i in range(1, len(price) - 1):
+    if price[i] <= price[i + 1]:
+        price[i + 1] = price[i]
+
+for j in range(len(length)):
+    money += length[j] * price[j]
+
+print(money)
