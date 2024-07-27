8a91af61554aae2141a65477fe5b074eb71a7983
Kim GwangYeong <gospotb1607@gmail.com>
2024-07-27 15:10:43 +0900

    [Bronze I] Title: 최소공배수, Time: 4036 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -0,0 +1,12 @@
+import sys
+
+t = int(sys.stdin.readline())
+for _ in range(t):
+    a, b = map(int,sys.stdin.readline().split())
+    answer = 1
+    for i in range(2, min(a,b)+1):
+        while a % i == 0 and b % i == 0:
+            answer *= i
+            a, b = a//i, b//i
+    answer *= a * b
+    print(answer)
