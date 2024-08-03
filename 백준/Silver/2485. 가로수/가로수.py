9a52ecbd7188f0df931c64093dd0b52d57a88f4f
Kim GwangYeong <gospotb1607@gmail.com>
2024-08-04 06:55:56 +0900

    [Silver IV] Title: 가로수, Time: 112 ms, Memory: 34972 KB -BaekjoonHub

---
+++ 백준/Silver/2485. 가로수/가로수.py
@@ -0,0 +1,28 @@
+import sys
+
+
+def GCD(a, b):
+    while b != 0:
+        a, b = b, a%b
+    return a
+
+
+n = int(sys.stdin.readline())
+m = []
+count, cc = 0, 0
+
+for i in range(n):
+    m.append(int(sys.stdin.readline()))
+    
+m.sort()
+
+l = m[1] - m[0]
+
+for i in range(len(m) - 1):
+    l = GCD(m[i + 1] - m[i], l)
+    
+for i in range(1, len(m)):
+    count += (m[i] - m[i-1]) / l - 1
+    
+
+print(int(count))
