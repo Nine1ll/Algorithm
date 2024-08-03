b4a3022e02b95e0042fea890bec9ee7db3bfc866
Kim GwangYeong <gospotb1607@gmail.com>
2024-08-03 14:08:27 +0900

    [Gold IV] Title: 연구소, Time: 1336 ms, Memory: 34244 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,63 +1,45 @@
 import sys
+from collections import deque
 
-n, m = map(int, sys.stdin.readline().split())
-data = []
-temp = [[0] * m for _ in range(n)]
+input = sys.stdin.readline
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+maxv = 0
+safe_areas = []
+virus_areas = []
+directions = [(0,1),(-1,0),(0,-1),(1,0)]
+for i in range(n):
+    for j in range(m):
+        if arr[i][j] == 0:
+            safe_areas.append((i, j))
+        elif arr[i][j] == 2:
+            virus_areas.append((i, j))
+def bfs(): # 바이러스 번지는 공간 체크 
+    ch = [[False]*m for _ in range(n)]
+    q = deque(virus_areas)
+    cnt = len(safe_areas)-3 # 벽 개수
+    for i, j in virus_areas:
+        ch[i][j] = True
+    while q:
+        x, y = q.popleft()
+        for dx, dy in directions:
+            xx, yy = x+dx, y+dy
+            if 0 <= xx < n and 0 <= yy < m and not arr[xx][yy] and not ch[xx][yy]:
+                ch[xx][yy] = True
+                q.append((xx, yy))
+                cnt -= 1
+    return cnt
 
-for _ in range(n):
-    data.append(list(map(int, sys.stdin.readline().rstrip().split())))
-
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
-
-result = 0
-
-def move_check(x,y):
-    return 0 <= x < n and 0 <= y < m
-
-
-def virus(x,y):
-    for i in range(4):
-        nx, ny = x + dx[i], y + dy[i]
-        if move_check(nx,ny):
-            if temp[nx][ny] == 0:
-                temp[nx][ny] = 2
-                virus(nx, ny)
-
-
-def get_score():
-    score = 0
-    for i in range(n):
-        for j in range(m):
-            if temp[i][j] == 0:
-                score += 1
-    return score
-
-
-def dfs(count):
-    global result
-    if count == 3:
-        for i in range(n):
-            for j in range(m):
-                temp[i][j] = data[i][j]
-
-        for i in range(n):
-            for j in range(m):
-                if temp[i][j] == 2:
-                    virus(i,j)
-
-        result = max(result, get_score())
+def dfs(l, start):
+    global maxv
+    if l == 3:
+        maxv = max(maxv, bfs())
         return
+    for i in range(start, len(safe_areas)):
+        x, y = safe_areas[i]
+        arr[x][y] = 1
+        dfs(l+1, i+1)
+        arr[x][y] = 0
 
-    for i in range(n):
-        for j in range(m):
-            if data[i][j] == 0:
-                data[i][j] = 1
-                count += 1
-                dfs(count)
-                data[i][j] = 0
-                count -= 1
-
-
-dfs(0)
-print(result)
+dfs(0, 0)
+print(maxv)
