8835e94f97b54686ef6b784c22ca7b6f79df306c
Kim GwangYeong <gospotb1607@gmail.com>
2024-08-10 17:39:25 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 6496 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -4,16 +4,22 @@ from collections import deque
 n, k = map(int, sys.stdin.readline().split())
 
 josephus = deque([i for i in range(1, n + 1)])
+length = len(josephus)
 cnt = 0
-
-answer = []
+print_count = 0
 while josephus:
+    if length == 1:
+        print(f"<{josephus.popleft()}>")
+        break
     cnt += 1
     p = josephus.popleft()
     if cnt % k == 0:
-        answer.append(p)
+        print_count += 1
+        if print_count == 1:
+            print(f"<{p}",end=", ")
+        elif print_count == n:
+            print(f"{p}>",end="")
+        else:
+            print(f"{p}", end=", ")
     else:
         josephus.append(p)
-
-result = ", ".join(map(str,answer))
-print("<" +result +">")
