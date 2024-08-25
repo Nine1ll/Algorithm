95f1604d8b3a88689db94a859fbc65df140d3401
Kim GwangYeong <gospotb1607@gmail.com>
2024-08-25 17:08:41 +0900

    [Silver III] Title: 달팽이, Time: 980 ms, Memory: 78000 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.py
+++ 백준/Silver/1913. 달팽이/달팽이.py
@@ -1,33 +1,26 @@
-# https://www.acmicpc.net/problem/1913
 import sys
 
-N = int(sys.stdin.readline())
-number = int(sys.stdin.readline())
+n = int(sys.stdin.readline())
+find_num = int(sys.stdin.readline())
 
-curr_y = 0  # 시작 y (0부터 시작 해서 그림)
-curr_x = 0  # 시작 X
-curr_dir = 0  # 어디로 가는지 연결 해주는 index 값
-direction = [[1, 0], [0, 1], [-1, 0], [0, -1]]  # down, right, up, left
-snail = [[0 for _ in range(N)] for _ in range(N)]
+direction = [(0, 1), (1, 0), (0, -1), (-1, 0)] # 하 좌 상 우
+snail = [[0] * n for _ in range(n)]
+visited = [[False] * n for _ in range(n)]
+x, y = 0, 0
+root = 0
 
-start = N * N  # 원소 값
-solution = list()
+result_coordinate = []
+for i in range(n**2, 0, -1):
+    if i == find_num:
+        result_coordinate = [x, y]
+    snail[y][x] = i
+    dx, dy = x + direction[root][0], y + direction[root][1]
+    if dx >= n or dx < 0 or dy >= n or dy < 0 or snail[dy][dx] != 0:
+        root = (root+1) % 4
 
-for _ in range(N * N):
-    if start == number:
-        solution = [curr_y + 1, curr_x + 1]
-    snail[curr_y][curr_x] = start
-    next_y = curr_y + direction[curr_dir][0]
-    next_x = curr_x + direction[curr_dir][1]
-    if next_y < 0 or next_x < 0 or next_y >= N or next_x >= N or snail[next_y][next_x] != 0:
-        curr_dir += 1
-        curr_dir %= 4
+    x, y = x + direction[root][0], y + direction[root][1]
 
-    curr_y = curr_y + direction[curr_dir][0]
-    curr_x = curr_x + direction[curr_dir][1]
-    start -= 1
 
-for lst in snail:
-    print(*lst)
-
-print(solution[0], solution[1])
+for s in snail:
+    print(*s)
+print(result_coordinate[1] + 1, result_coordinate[0] + 1)
